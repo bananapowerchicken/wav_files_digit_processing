916f99eb22a109abcb75e334edd2a4c2108eae1c
LobashovV <lobashovrff@gmail.com>
2020-07-30 21:23:31 +0300

    added files from work, can open wav files here

---
+++ wav_files_digit_proc/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+#include <fstream>
+
+struct Header
+{
+	char p1[4];
+	unsigned long chunkSize;
+	char format[4];
+	char subchunk1Id[4];
+	unsigned long subchunk1Size;
+	unsigned short audioFormat;
+	unsigned short numChannels;
+	unsigned long sampleRate;
+	unsigned long byteRate;
+	unsigned short blockAlign;
+	unsigned short bitsPerSample;
+	char subchunk2Id[4];
+	unsigned long subchunk2Size;
+};
+
+int main( int argc, char ** argv )
+{
+	char buf[44] = {}; // массив-буфер для считывания
+	std::ifstream f("3285.wav", std::ios::binary);
+	if (f.is_open()) std::cout << "file is opened"<< '\n'; // файл открыт, он открывается
+
+	Header h;
+	std::cout << sizeof(h);
+
+	f >> h.p1 >> h.chunkSize >> h.format >> h.subchunk1Id >> h.subchunk1Size>> h.audioFormat >> h.numChannels >> h.sampleRate >> h.byteRate >> h.blockAlign >> h.bitsPerSample >> h.subchunk2Id >> h.subchunk2Size;
+	f.read( (char*) buf, sizeof(buf));
+	f.read( (char*) h.p1, sizeof(h.p1) );
+	
+
+	
+
+	return 0;
+}
+
