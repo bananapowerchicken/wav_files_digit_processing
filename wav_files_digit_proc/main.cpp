db0ec4525cfbe88310248ea1bac0b311e472e8c2
LobashovV <lobashovrff@gmail.com>
2020-07-31 23:36:03 +0300

    read data part of wav file

--- wav_files_digit_proc/main.cpp
+++ wav_files_digit_proc/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream> 
-#include <fstream>
+#include <fstream> 
 
 struct Header
 {
@@ -20,22 +20,35 @@ struct Header
 
 int main( int argc, char ** argv )
 {
-	char buf[44] = {}; // массив-буфер для считывания
+	
 	std::ifstream f("fspeech.wav", std::ios::binary);
 	if (f.is_open()) std::cout << "file is opened"<< '\n'; // файл открыт, он открывается
 
 	Header h;
-	std::cout << sizeof(h);
+	/*std::cout << sizeof(h);*/
 
 	
 	//f.read( (char*) buf, sizeof(buf));
 	f.read( (char*) &h, sizeof(h) );
+	
+	short * data_buf = new short[h.subchunk2Size>>1]; //  динамический массив-буфер для считывания информации о файле
+	f.read( (char*) data_buf, h.subchunk2Size);
+	std::cout << *(data_buf + 2) << '\n';
 
+	short* data_buf_out = new short[h.subchunk2Size];
+	for (int i = 0; i < sizeof(data_buf_out); i++)
+	{
+
+	}
+
+
+
+	delete data_buf; // 
 
 	
-
-	
-
+	system("pause");
 	return 0;
+
+	
 }
 
