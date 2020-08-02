ee0d2d7a0adadc42d3252734aac9cbd8b9057493
LobashovV <lobashovrff@gmail.com>
2020-08-02 22:40:06 +0300

    made new signal with another amplitude

--- wav_files_digit_proc/main.cpp
+++ wav_files_digit_proc/main.cpp
@@ -1,7 +1,10 @@
+
+
 #include <iostream> 
 #include <fstream> 
 
-struct Header
+// заголовок показывает проигрывателю, как надо читать данный аудиофайл. 
+struct Header 
 {
 	char p1[4];
 	unsigned long chunkSize;
@@ -25,25 +28,26 @@ int main( int argc, char ** argv )
 	if (f.is_open()) std::cout << "file is opened"<< '\n'; // файл открыт, он открывается
 
 	Header h;
-	/*std::cout << sizeof(h);*/
+		
+	f.read( (char*) &h, sizeof(h) ); // считывание идет по адресу
+	unsigned long size_of_data_buf = h.subchunk2Size >> 1;
+	short * data_buf = new short[size_of_data_buf/*h.subchunk2Size >> 1*/]; //  динамический массив-буфер для считывания информации о файле
+	std::cout << data_buf << '\n';
 
-	
-	//f.read( (char*) buf, sizeof(buf));
-	f.read( (char*) &h, sizeof(h) );
-	
-	short * data_buf = new short[h.subchunk2Size>>1]; //  динамический массив-буфер для считывания информации о файле
 	f.read( (char*) data_buf, h.subchunk2Size);
-	std::cout << *(data_buf + 2) << '\n';
+	std::cout << h.subchunk2Size <<" " << sizeof(data_buf)<< " " << size_of_data_buf << '\n';
 
-	short* data_buf_out = new short[h.subchunk2Size];
-	for (int i = 0; i < sizeof(data_buf_out); i++)
+	short * data_buf_out = new short[h.subchunk2Size >> 1];
+	for (int i = 0; i < size_of_data_buf; i++)
 	{
-
+		data_buf_out[i] = data_buf[i] / 2; // уменьшаю амплитуду в два раза
+		std::cout << data_buf[i] << " -> " << data_buf_out[i] << '\n';
 	}
 
 
 
 	delete data_buf; // 
+	delete data_buf_out;
 
 	
 	system("pause");
