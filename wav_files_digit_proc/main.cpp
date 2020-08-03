cc9161b5cccf27c9125ea5b1a438a9f700aeea65
LobashovV <lobashovrff@gmail.com>
2020-08-03 21:51:00 +0300

    удалось обрезать файл и уменьшить амплитуду сигнала, получилась обработка, ураgit add .!

--- wav_files_digit_proc/main.cpp
+++ wav_files_digit_proc/main.cpp
@@ -30,21 +30,31 @@ int main( int argc, char ** argv )
 	Header h;
 		
 	f.read( (char*) &h, sizeof(h) ); // считывание идет по адресу
-	unsigned long size_of_data_buf = h.subchunk2Size >> 1;
-	short * data_buf = new short[size_of_data_buf/*h.subchunk2Size >> 1*/]; //  динамический массив-буфер дл€ считывани€ информации о файле
+	
+	short * data_buf = new short[h.subchunk2Size >> 1]; //  динамический массив-буфер дл€ считывани€ информации о файле
 	std::cout << data_buf << '\n';
 
 	f.read( (char*) data_buf, h.subchunk2Size);
-	std::cout << h.subchunk2Size <<" " << sizeof(data_buf)<< " " << size_of_data_buf << '\n';
+	
+	Header hout = h;
+	hout.subchunk2Size = h.subchunk2Size / 2;
+	short * data_buf_out = new short[hout.subchunk2Size >> 1];
 
-	short * data_buf_out = new short[h.subchunk2Size >> 1];
-	for (int i = 0; i < size_of_data_buf; i++)
+	// редактирование сигнала по отсчетам
+	for (int i = 0; i < (hout.subchunk2Size >> 1); i++)
 	{
-		data_buf_out[i] = data_buf[i] / 2; // уменьшаю амплитуду в два раза
-		std::cout << data_buf[i] << " -> " << data_buf_out[i] << '\n';
+		data_buf_out[i] = data_buf[i]; // измен€ю сэмплы
+		//std::cout << data_buf[i] << " -> " << data_buf_out[i] << '\n';
 	}
+	
 
+	std::ofstream fout("fspeech_edited.wav", std::ios::binary); // новый отредактированный файл с измененной амплитудой
+	fout.write((char*) &hout, sizeof(hout)); // записали новый заголовок
+	fout.write((char*) data_buf_out, hout.subchunk2Size); // записали саму информационную часть
 
+	// чистка
+	f.close();
+	fout.close();
 
 	delete data_buf; // 
 	delete data_buf_out;
